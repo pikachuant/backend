baa0b3403a34ed98bc110ba90f0196a36d945e9c
Satyajit <satyajitswain78@gmail.com>
2026-07-14 14:39:55 +0530

    Updated

--- utils/asyncHandler.js
+++ utils/asyncHandler.js
@@ -2,7 +2,13 @@ const asyncHandler=(requesthandler)=>{
    return (req,res,next)=>{
     Promise
     .resolve(requesthandler(req,res,next))
+    //if promised resolve then ok when not resolved it saying "throw" this word simply said the promise is rejected 
+    //this word "throw" only work on promise to say the promise that the promise is rejected and then catch part will recoeved the
+    //error object created by Apierror object creator
+    //error=error object conatin all details whihc is a truthy value
     .catch((error)=>next(error))
+    //next() is just simple next execution but 
+    //next(truthy value/any value) give instruction to stop further execution to express
    }
 }
 
