22766a6e91c17541dd56d9c80fe8094ac8e4c963
Satyajit <satyajitswain78@gmail.com>
2026-09-16 17:14:13 +0530

    add file-type

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -65,7 +65,6 @@ const uploadOnCloudiNary=async function(localFilepath) {
             }
         )
         const url=`https://antonpklive.online/backend/${objectName}`
-        console.log(detected)
         return url
     } catch (error) {
         console.log("Upload error on Minio",error);
