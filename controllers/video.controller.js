8c4085c7e2ce861767ab95a73c9ab087b853cd51
Satyajit <satyajitswain78@gmail.com>
2026-09-23 13:26:38 +0530

    hello

--- controllers/video.controller.js
+++ controllers/video.controller.js
@@ -306,7 +306,6 @@ export const findVideoByName = asyncHandler(async function (req, res) {
 export const getvideoById = asyncHandler(async function (req, res) {
   const videoId = req.params?.videoId;
   const id = req.user?._id;
-
   console.log("user-id",id);
   
 
